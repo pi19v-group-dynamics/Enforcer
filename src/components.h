5d12abb130ef88f673c190207bc4396b61019cf4
66-99 <emoxypine@protonmail.com>
2021-03-27 12:30:42 +0300

    [#11] Создание компоненты юнита

--- src/components.h
+++ src/components.h
@@ -1,9 +1,50 @@
 #ifdef COMPONENTS_H
 #define COMPONENTS_H
 
+typedef enum
+{
+	TEAM_NONE = 0
+	TEAM_RED,
+	TEAM_GREEN,
+	TEAM_BLUE
+}
+unit_team_t;
+
+typedef enum
+{
+	WEAPON_NONE = 0,
+	WEAPON_MACHINE_GUN,
+	WEAPON_AUTOMATIC,
+	WEAPON_RIFLE,
+	WEAPON_LAUNCHER,
+	WEAPON_TANK_CANNON
+}
+unit_weapon_t;
+
 typedef struct
 {
-	
+	PROTECTION_NONE = 0,		/* for target */
+	PROTECTION_BULLETPROOF_VEST	/* for units*/
+	PROTECTION_COMPOSITE_ARMOUR	/* for tanks */
+}
+unit_protection_t;
+
+typedef union
+{
+	struct
+	{
+		int health;
+		int view_range;
+		unit_team_t team;
+		unit_weapon_t weapon;
+		unit_protection_t protection;
+	} unit;
+
+	struct
+	{
+		float capture_level;
+		unit_team_t captured;
+	} point;
 }
 unit_comp_t;
 
