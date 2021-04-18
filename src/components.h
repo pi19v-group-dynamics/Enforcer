21a4b8012e891633e564a1c98a0fd618f98783f4
ooichu <50176726+ooichu@users.noreply.github.com>
2021-04-18 18:44:13 +0300

    Исправления в components.h

--- src/components.h
+++ src/components.h
@@ -96,9 +96,52 @@ void rend_system(ecs_world_t* world, ecs_entity_t ent);
  * Unit component
  *****************************************************************************/
 
-typedef struct unit_comp
+typedef enum
 {
-	
+	TEAM_NONE = 0,
+	TEAM_RED,
+	TEAM_GREEN,
+	TEAM_BLUE
+}
+unit_team_t;
+
+typedef enum
+{
+	WEAPON_NONE = 0,
+	WEAPON_MACHINE_GUN,
+	WEAPON_AUTOMATIC,
+	WEAPON_RIFLE,
+	WEAPON_LAUNCHER,
+	WEAPON_TANK_CANNON
+}
+unit_weapon_t;
+
+typedef enum 
+{
+	PROTECTION_NONE = 0,         /* for target */
+	PROTECTION_BULLETPROOF_VEST, /* for units */
+	PROTECTION_COMPOSITE_ARMOUR  /* for tanks */
+}
+unit_protection_t;
+
+typedef union
+{
+	struct
+	{
+		int health;
+		int view_range;
+		unit_team_t team;
+		unit_weapon_t weapon;
+		unit_protection_t protection;
+	}
+	unit;
+
+	struct
+	{
+		float capture_level;
+		unit_team_t captured;
+	}
+	point;
 }
 unit_comp_t;
 
